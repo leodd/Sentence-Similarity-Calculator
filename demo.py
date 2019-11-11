2c05334bb5c2e62e36b46350b2f5a56f62f52895
leodd <410528518@qq.com>
2019-11-11 01:03:47 -0600

    updates

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 from utils import *
+from nltk.corpus import wordnet
 
-print(load_data('data/test-set.txt', encoding='utf8'))
+
+synset = wordnet.synsets("Travel")
+print('Word and Type : ' + synset[0].name())
+print('Synonym of Travel is: ' + synset[0].lemmas()[0].name())
+print('The meaning of the word : ' + synset[0].definition())
+print('Example of Travel : ' + str(synset[0].examples()))
+
+# print(load_data('data/test-set.txt', encoding='utf8'))
