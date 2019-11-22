698089a5c0c2bed4a01b0df0417421efc2c7bfac
leodd <410528518@qq.com>
2019-11-22 17:49:18 -0600

    updates

--- demo.py
+++ demo.py
@@ -10,12 +10,12 @@ for _, item in data.items():
     item['Tokenized Sentence1'] = tokenized_sentence(item['Sentence1'])
     item['Tokenized Sentence2'] = tokenized_sentence(item['Sentence2'])
 
-for _, item in data.items():
-    item['Pos-tagged Sentence1'] = pos_tagged_sentence(item['Tokenized Sentence1'])
-    item['Pos-tagged Sentence2'] = pos_tagged_sentence(item['Tokenized Sentence2'])
+# for _, item in data.items():
+#     item['Pos-tagged Sentence1'] = pos_tagged_sentence(item['Tokenized Sentence1'])
+#     item['Pos-tagged Sentence2'] = pos_tagged_sentence(item['Tokenized Sentence2'])
+#
+# for _, item in data.items():
+#     item['Lemmatized Sentence1'] = pos_tagged_sentence(item['Pos-tagged Sentence1'])
+#     item['Lemmatized Sentence2'] = pos_tagged_sentence(item['Pos-tagged Sentence2'])
 
-for _, item in data.items():
-    item['Lemmatized Sentence1'] = pos_tagged_sentence(item['Pos-tagged Sentence1'])
-    item['Lemmatized Sentence2'] = pos_tagged_sentence(item['Pos-tagged Sentence2'])
-
-print(data)
+print(stringized_data(data))
--- utils.py
+++ utils.py
@@ -1,9 +1,21 @@
-import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from nltk.parse.corenlp import CoreNLPServer
+
+
+def stringized_data(data):
+    str_list = list()
+    for key, item in data.items():
+        str_list.append(str(key))
+        for content_key, content in item.items():
+            str_list.append('\n\t{}: {}'.format(content_key, content))
+        str_list.append('\n')
+    return ''.join(str_list)
 
 
 def tokenized_sentence(s):
-    return nltk.word_tokenize(s)
+    return word_tokenize(s)
 
 
 def pos_tagged_sentence(l):
-    return nltk.pos_tag(l)
+    return
