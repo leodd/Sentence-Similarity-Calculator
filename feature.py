640bd227e7b4c2375c2c88925a3dd0fc242f446f
YongliangYang <598931066@qq.com>
2019-12-05 03:53:17 -0600

    updates

--- feature.py
+++ feature.py
@@ -1,5 +1,6 @@
 from word_similarity import *
 import numpy as np
+from nltk.corpus import stopwords
 def sentence_union(l1,l2):  #l1 and l2 should be lemma-post list
     s1 = set(l1)
     s2 = set(l2)
@@ -7,14 +8,29 @@ def sentence_union(l1,l2):  #l1 and l2 should be lemma-post list
     # v1, v2 = [], []
     return  list(union_set) #,list(s1) ,list(s2)
 
-def tf_idf_feature(l1, l2): #l1 and l2 should be lemma-post list
+def unionWithStopWord(l1,l2):
+    # l1 and l2 should be lemma-post list
+    # return the two removed stopword set without tag and their union set
     s1 = set(l1)
     s2 = set(l2)
-    # union_set = s1 | s2
-    v1,v2 =[],[]
-    union_set  = sentence_union(l1,l2)
+    # sw contains the list of stopwords
+    sw = stopwords.words('english')
+
+    # remove stop words from string
+    X_set = {w[0] for w in s1 if w[0] not in sw}
+    Y_set = {w[0] for w in s2 if w[0] not in sw}
 
     # form a set containing keywords of both strings
+    union_set = X_set.union(Y_set)
+    return union_set,X_set,Y_set
+
+
+def tf_idf_feature(l1, l2): #l1 and l2 should be lemma-post list
+    # [('a', 'DT'), ('25', 'CD'), ('percent', 'NN')
+
+    v1,v2 =[],[]
+    union_set ,s1,s2 = unionWithStopWord(l1,l2)
+
     for w in union_set:
         if w in s1:
             v1.append(1)
@@ -75,8 +91,12 @@ def semantic_similary(l1,l2):
 
 
 if __name__ == '__main__':
-    result= semantic_similary(
-        [('a', 'DT'), ('25', 'CD'), ('percent', 'NN'), ('increase', 'NN'), ('would', 'MD')],
-        [('the', 'DT'), ('25', 'CD'), ('percent', 'NN'), ('hike', 'NN'), ('take', 'VBZ')]
+    u,s1,s2= unionWithStopWord(
+[('a', 'DT'), ('25', 'CD'), ('percent', 'NN'), ('increase', 'NN'), ('would', 'MD'), ('raise', 'VB'), ('undergraduate', 'JJ'), ('tuition', 'NN'), ('to', 'TO'), ('about', 'IN'), ('5,247', 'CD'), ('annually', 'RB'), ('include', 'VBG'), ('miscellaneous', 'JJ'), ('campus-based', 'JJ'), ('fee', 'NNS')]
+,
+        [('annual', 'JJ'), ('uc', 'NN'), ('undergraduate', 'JJ'), ('tuition', 'NN'), ('to', 'TO'), ('4,794', 'CD'),
+         ('and', 'CC'), ('graduate', 'JJ'), ('fee', 'NNS'), ('to', 'TO'), ('5,019', 'CD')]
     )
-    print(result)
+print(u)
+print(s1)
+print(s2)
