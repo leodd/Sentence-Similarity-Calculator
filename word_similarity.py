021d0ddf3eab8e8ec78610c66195ef27dd363621
leodd <410528518@qq.com>
2019-12-04 20:30:25 -0600

    updates

--- word_similarity.py
+++ word_similarity.py
@@ -1,5 +1,6 @@
 from nltk.corpus import wordnet as wn
 from nltk.corpus import wordnet_ic
+import numpy as np
 from itertools import product
 
 
@@ -30,3 +31,42 @@ def wup_similarity(w1, w2, pos=None):
         res = max(res, 0 if d is None else d)
 
     return res
+
+
+def wordset_similarity(ws1, ws2):
+    # word set is a set with word-pos tuple
+    # e.g. {('apple', 'NN'), ('car', 'NN')}
+    ws_common = ws1 | ws2
+    ws_vector = list(ws_common)
+
+    v1 = list()
+    v2 = list()
+
+    for word in ws_vector:
+        _, max_similarity = most_similar_word(word, ws1)
+        v1.append(max_similarity)
+
+        _, max_similarity = most_similar_word(word, ws2)
+        v2.append(max_similarity)
+
+    v1 = np.array(v1)
+    v2 = np.array(v2)
+
+    return np.sum(v1 * v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+
+def most_similar_word(target, ws):
+    max_matching = None
+    max_similarity = 0
+
+    for w in ws:
+        similarity = 1 / (1.1 - wup_similarity(target[0], w[0]))
+        if similarity >= max_similarity:
+            max_matching = w
+            max_similarity = similarity
+
+    return max_matching, max_similarity
+
+
+if __name__ == '__main__':
+    print(wordset_similarity({('red', 'JJ'), ('car', 'NN')}, {('kill', 'VB'), ('egg', 'NN'), ('cat', 'NN')}))
